e4d21e6672615f147d4028dbe34d7d404c14ced0
Jaromrax <jaromrax@gmail.com>
2017-06-05 19:53:01 +0200

    float text on pic, 10 points lower

--- opencv_stuff/webcam.py
+++ opencv_stuff/webcam.py
@@ -322,15 +322,15 @@ while True:
             meanstd=statistics.stdev( meanlist[0:10] )
         meanmean=sum( meanlist) / len( meanlist )
 
-        text="{:4.0f}".format(abs(meanmean-mean)/meanstd)
+        text="{:5.1f}".format(abs(meanmean-mean)/meanstd)
         textcolor=(255, 0, 0) #blue
 
-        cv2.putText(frame, "{}".format(text), (xoff, yoff),
-           cv2.FONT_HERSHEY_SIMPLEX, 0.6, textcolor , 2)
+        cv2.putText(frame, "{}".format(text), (xoff, yoff+10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, textcolor , 1)
         if abs(meanmean-mean)/meanstd>args.motionmode:
-            textcolor=(0,255,0)
-            cv2.putText(frame, "{}".format(text), (xoff, yoff),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, textcolor , 2)
+            textcolor=(0,255,0) #green
+            cv2.putText(frame, "{}".format(text), (xoff, yoff+10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, textcolor , 1)
             print( '    {}  M={:.3f} +- {:.3f} ... {:.3f} {:.1f}'.format(len(meanlist),
                                                                          meanmean,meanstd, mean, abs(meanmean-mean)/meanstd ) ,
                    end='   ')
